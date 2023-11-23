9ae332193bad6306b5d48d66045338cb760fcbca
HannahMcCabe31 <hmccabe.hm@gmail.com>
2023-11-23 17:28:21 +0000

    working on connection with server

--- back-end/app.js
+++ back-end/app.js
@@ -1,18 +1,20 @@
 // Import required modules
-import express from "express";  // Import the Express framework
-import morgan from "morgan";    // Import the Morgan middleware for logging
-import cors from "cors";        // Import the CORS middleware for handling cross-origin requests
-
-import { workshopsRoutes } from "./routes/workshopsRoutes.js";  // Import the routes for workshops (custom module)
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import { workshopsRoutes } from "./routes/workshopsRoutes.js";
 
 // Create an Express application instance
 export const app = express();
 
 // Configure Cross-Origin Resource Sharing (CORS) for the application
 app.use(
-    cors(
-
-    )
+  cors({
+    origin: "*", // You can replace "*" with your frontend's domain in production
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+    optionsSuccessStatus: 204,
+  })
 );
 
 // Use the Morgan middleware for request logging in 'dev' format
@@ -26,3 +28,32 @@ app.use("/workshops", workshopsRoutes);
 
 // (Optional) You can define more routes or middleware for handling "errors" here if needed
 // app.use("/errors", errorsRoutes);
+
+// // Import required modules
+// import express from "express";  // Import the Express framework
+// import morgan from "morgan";    // Import the Morgan middleware for logging
+// import cors from "cors";        // Import the CORS middleware for handling cross-origin requests
+
+// import { workshopsRoutes } from "./routes/workshopsRoutes.js";  // Import the routes for workshops (custom module)
+
+// // Create an Express application instance
+// export const app = express();
+
+// // Configure Cross-Origin Resource Sharing (CORS) for the application
+// app.use(
+//     cors(
+
+//     )
+// );
+
+// // Use the Morgan middleware for request logging in 'dev' format
+// app.use(morgan("dev"));
+
+// // Parse incoming JSON requests and make the data available in req.body
+// app.use(express.json());
+
+// // Define a route for handling requests related to "workshops"
+// app.use("/workshops", workshopsRoutes);
+
+// // (Optional) You can define more routes or middleware for handling "errors" here if needed
+// // app.use("/errors", errorsRoutes);
